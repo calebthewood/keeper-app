66958344ae1053941b349297e45f51f0e9b7c151
calebthewood <calebwood.cs@gmail.com>
2022-02-28 21:59:46 -0800

    second commit

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "./Header";
+import Note from "./Note"
+import Footer from "./Footer";
+import notes from "../notes";
+
+
+
+function App() {
+    return (
+        <div>
+            <Header />
+            {notes.map((notes) => ( /* arrow func in map */
+                <Note 
+                    key={notes.key} 
+                    title={notes.title} 
+                    content={notes.content} 
+                />
+    ))};
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
